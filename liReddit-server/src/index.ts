3b66b942db1b643d880c13edeb8349254fe4aa12
Andrej Germič <andrej.germic@gmail.com>
2021-01-16 12:04:39 +0100

    Setup

---
+++ liReddit-server/src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello lorld");
+/*
+Tutorial project using technologies: React - TypeScript - GraphQL - URQL/Apollo - Node.js - PostgreSQL - MikroORM/TypeORM - Redis - Next.js - TypeGraphQL - Chakra. Guided by Ben Awad.
+
+liReddit---Fullstack-React-GraphQL-TypeScript-Tutorial
+*/
