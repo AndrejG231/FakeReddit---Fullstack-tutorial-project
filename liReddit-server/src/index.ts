d3375e99e69eba6d856232a125fb71cb35c89821
Andrej Germič <andrej.germic@gmail.com>
2021-01-17 18:31:56 +0100

    GraphQl setup

--- liReddit-server/src/index.ts
+++ liReddit-server/src/index.ts
@@ -1,18 +1,33 @@
-import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
-// import { Post } from './entities/Post';
-import microConfig from './mikro-orm.config';
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver, PostResolver, UserResolver } from "./resolvers";
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'my first post'})
-    // await orm.em.persistAndFlush(post);
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts)
-}
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
 
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
---
+++ liReddit-server/src/resolvers/user.ts
@@ -0,0 +1,107 @@
+import { User } from "../entities";
+import { MyContext } from "src/types";
+import {
+  Resolver,
+  InputType,
+  Mutation,
+  Arg,
+  Field,
+  Ctx,
+  ObjectType,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater then 2",
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    const valid = await em.findOne(User, { username: options.username });
+    if (!valid) {
+      await em.persistAndFlush(user);
+      return { user };
+    }
+    return {
+      errors: [
+        {
+          field: "username",
+          message: "that username already exist",
+        },
+      ],
+    };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "that username doesn't exist",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorect password",
+          },
+        ],
+      };
+    }
+    return {
+      user,
+    };
+  }
+}
