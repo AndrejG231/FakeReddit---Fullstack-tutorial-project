31304e3abfdc8aa428b6177a5ca0a39be453b718
Andrej Germič <andrej.germic@gmail.com>
2021-01-24 11:42:28 +0100

    Finished password recovery

--- lireddit-web/src/pages/forgot-password.tsx
+++ lireddit-web/src/pages/forgot-password.tsx
@@ -1,11 +1,51 @@
-import { withUrqlClient } from 'next-urql';
-import React from 'react';
-import { createUrqlClient } from '../utilities/createUrqlCLient';
+import React, { useState } from "react";
+import { useForgotPasswordMutation } from "../generated/graphql";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utilities/createUrqlCLient";
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
-const ForgotPassword = () => {
-    return (
-        <div>Hello</div>
-    )
-}
+const ForgotPassword: React.FC<{}> = ({}) => {
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
+        }}
+      >
+        {({ isSubmitting }) => {
+          return complete ? (
+            <Box>
+              If an account with that email exist, we've sent you email.
+            </Box>
+          ) : (
+            <Form>
+              <InputField
+                name="email"
+                placeholder="email"
+                label="Email"
+                type="email"
+              />
+              <Button
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+                colorScheme="teal"
+              >
+                Send me a new password
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </Wrapper>
+  );
+};
 
-export default withUrqlClient(createUrqlClient)(ForgotPassword)
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
